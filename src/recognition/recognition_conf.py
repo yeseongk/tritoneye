0e860e3538741b553f93d02c0b6eb860892b6e05
Yeseong Kim <yek048@ucsd.edu>
2016-04-16 02:43:59 +0000

    Git ignore update

--- src/recognition/recognition_conf.py
+++ src/recognition/recognition_conf.py
@@ -13,10 +13,10 @@ class GapFillingMethod:
 # --------------------------------------------------
 # Image processing algorithm related
 BG_SUBTRACTOR = BackgroundSubtractor.MOG2
-NEGLECT_SHADOW = False
-FILTER_NOISE_SIZE = (2,2)
-GAP_FILLING_METHOD = GapFillingMethod.MEDIAN
-MIN_OBJECT_AREA = 150 # Note: It is strongly related to the frame size.
+NEGLECT_SHADOW = True
+FILTER_NOISE_SIZE = (1,1)
+GAP_FILLING_METHOD = GapFillingMethod.BILATERAL
+MIN_OBJECT_AREA = 80 # Note: It is strongly related to the frame size.
 
 # Tracking heuristic related
 RECENT_FRAMES_TO_TRACK = 4 
