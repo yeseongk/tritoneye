fa48323ece38138e341610cac7e03fc1b6edc80a
Yeseong Kim <yek048@ucsd.edu>
2016-05-25 21:10:46 +0000

    TCP communication module

---
+++ src/communication/comm_conf.py
@@ -0,0 +1,7 @@
+# Configuration for communication method
+
+# TCP socket configuration
+TCP_IP = '137.110.160.147' 
+#TCP_IP = '137.110.160.147' # local repo server
+TCP_PORT = 3000
+DEVICE_ID = "ID_00_FROM_PY"
